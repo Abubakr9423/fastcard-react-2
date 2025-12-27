a55afe9ae9cfa36ab86be6dd3df0607bc748b69c
MUHSIN2011 <muhsinnazarov21@gmail.com>
2025-12-27 23:59:10 +0500

    Finish Media Navbar,Footer; Update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,99 @@
-import React from 'react'
+import imgIphone from '../assets/hero_endframe__cvklg0xk3w6e_large 2.png'
+import LogoIphone from '../assets/1200px-Apple_gray_logo 1.png'
+import { MoveLeft, MoveRight, Search } from 'lucide-react'
+import { Input } from '@/components/ui/input'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className='max-w-337.5 m-auto my-2 md:px-0 px-3'>
+      <section className='flex md:flex-row flex-col'>
+        <aside className='md:flex hidden flex-col items-start gap-3 w-[20%]'>
+          <select name="" id="">
+            <option value="">Woman’s Fashion</option>
+          </select>
+          <select name="" id="">
+            <option value="">Woman’s Fashion</option>
+          </select>
+          <p>Electronics</p>
+          <p>Home & Lifestyle</p>
+          <p>Medicine</p>
+          <p>Sports & Outdoor</p>
+          <p>Baby’s & Toys</p>
+          <p>Groceries & Pets</p>
+          <p>Health & Beauty</p>
+        </aside>
+
+        <aside className='my-2 md:hidden block'>
+          <Input type="text" className='px-2' placeholder='Search' />
+          <div className='flex flex-wrap my-2 gap-2'>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Woman’s Fashion {'->'}</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Men’s Fashion {'->'}</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Electronics</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Home & Lifestyle</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Medicine</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Sports & Outdoor</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Baby’s & Toys</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Groceries & Pets</p>
+            <p className='px-2 py-2 rounded-sm bg-[#F5F5F5] '>Health & Beauty</p>
+          </div>
+        </aside>
+
+        <aside className='bg-black text-white md:w-[80%] rounded-sm flex md:flex-row flex-col md:gap-0 gap-3  md:px-20 md:py-0 py-10  items-center justify-between'>
+          <div className='flex flex-col justify-between gap-5'>
+            <div className='flex items-center gap-2'>
+              <img src={LogoIphone} alt="" />
+              <h1 >iPhone 14 Series</h1>
+            </div>
+            <div>
+              <h1 className='md:text-6xl text-2xl font-bold'>Up to 10% off Voucher</h1>
+            </div>
+            <div>
+              <p className='text-2xl'>Shop Now {'->'}</p>
+            </div>
+          </div>
+          <div className='md:w-140'>
+            <img className='w-full' src={imgIphone} alt="" />
+          </div>
+        </aside>
+      </section>
+      <section className='my-5'>
+        <div className='text-[#DB4444]  flex items-center gap-2'>
+          <div className='bg-[#DB4444] rounded-[4px]  py-2 px-1'>O</div>
+          <p className='font-bold text-xl'>Today’s</p>
+        </div>
+        <div className='flex md:my-0 my-5 md:items-center md:flex-row flex-col justify-between'>
+          <div className='flex md:flex-row flex-col md:items-center md:gap-25'>
+            <h1 className='text-5xl font-bold'>Flash Sales</h1>
+            <div className='flex items-center gap-2'>
+              <div>
+                <p className='font-bold'>Days</p>
+                <h1 className='text-5xl font-bold'>03</h1>
+              </div>
+              <span className='text-3xl font-bold mt-5 text-[#DB4444]'>:</span>
+              <div>
+                <p className='font-bold'>Hours</p>
+                <h1 className='text-5xl font-bold'>23</h1>
+              </div>
+              <span className='text-3xl font-bold mt-5 text-[#DB4444]'>:</span>
+              <div>
+                <p className='font-bold'>Minutes</p>
+                <h1 className='text-5xl font-bold'>19</h1>
+              </div>
+              <span className='text-3xl font-bold mt-5 text-[#DB4444]'>:</span>
+              <div>
+                <p className='font-bold'>Seconds</p>
+                <h1 className='text-5xl font-bold'>19</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className='md:flex hidden items-center gap-2'>
+            <button className='bg-[#F5F5F5] rounded-full p-4'><MoveLeft /></button>
+            <button className='bg-[#F5F5F5] rounded-full p-4'><MoveRight /></button>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,64 +1,75 @@
-import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import img1 from "../assets/Group 1116606595 (1).png"
 import { Input } from '@/components/ui/input'
 import img2 from "../assets/Wishlist (1).png"
 import img3 from "../assets/Cart1 (1).png"
 import img4 from "../assets/Frame 741.png"
+import { Menu, User } from 'lucide-react'
 
 const Layout = () => {
   return (
     <>
-      <header>
-        <nav className='flex items-center justify-evenly'>
-          <img src={img1} alt="" />
-          <div className='flex gap-5'>
+      <header className='border'>
+        <nav className='flex items-center max-w-337.5 m-auto md:px-0 px-3  justify-between py-2'>
+          <div className='md:hidden flex items-center gap-5'>
+            <Menu />
+            <h1 className='text-2xl font-bold'>Exclusive</h1>
+          </div>
+          <img className='md:block hidden' src={img1} alt="" />
+          <div className='md:flex gap-6  hidden font-medium'>
             <Link to="/home">Home</Link>
             <Link to="/contact">Contact</Link>
             <Link to="/about">About</Link>
-            <Link to="/sign">Sign Up</Link>
+            <Link to="/register">Sign Up</Link>
           </div>
           <div className='flex gap-5 items-center'>
-            <Input type="search" placeholder='search' className='w-[243px]' />
-            <img src={img2} alt="" />
+            <Input type="search" placeholder='search' className='md:block w-70 hidden' />
+            <img className='md:block hidden' src={img2} alt="" />
             <img src={img3} alt="" />
+            <User className='md:block hidden' />
           </div>
         </nav>
       </header>
       <div>
         <Outlet />
       </div>
-      <footer className='bg-[#000000] text-[#FAFAFA] flex justify-evenly p-20 '>
-        <div className='flex flex-col gap-5'>
-          <h1 className='text-[24px]'>Exclusive</h1>
-          <h1>Subscribe</h1>
-          <p>Get 10% off your first order</p>
-          <Input type="search" placeholder='search' className='w-[243px]' />
-        </div>
-        <div className='flex flex-col gap-5'>
-          <h1 className='text-[20px]'>Support</h1>
-          <p>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
-          <p>exclusive@gmail.com</p>
-          <p>+88015-88888-9999</p>
-        </div>
-        <ul className='flex flex-col gap-5'>
-          <li className='text-[20px]'>Account</li>
-          <li>My Account</li>
-          <li>Cart</li>
-          <li>Wishlist</li>
-          <li>Shop</li>
-        </ul>
-        <ul className='flex flex-col gap-5'>
-          <li className='text-[20px]'>Quick Link</li>
-          <li>Privacy Policy</li>
-          <li>Terms Of Use</li>
-          <li>FAQ</li>
-          <li>Contact</li>
-        </ul>
-        <div className='flex flex-col gap-5'>
-          <h1 className='text-[20px]'>Social </h1>
-          <img src={img4} alt="" />
-        </div>
+      <footer className='bg-[#000000] text-[#FAFAFA]  md:p-20 p-3'>
+        <section className='max-w-337.5 m-auto  flex md:flex-row flex-col justify-between'>
+          <div className='flex flex-col gap-5'>
+            <h1 className='md:text-[24px] text-3xl font-bold'>Exclusive</h1>
+            <h1>Subscribe</h1>
+            <p>Get 10% off your first order</p>
+            <Input type="search" placeholder='search' className='w-[243px]' /><br />
+          </div>
+          <div className='flex flex-col gap-5'>
+            <h1 className='md:text-[20px] md:font-black text-3xl'>Support</h1>
+            <p className='md:w-auto w-50'>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
+            <p>exclusive@gmail.com</p>
+            <p>+88015-88888-9999</p>
+          </div><br />
+          <div className='flex md:gap-25 gap-25 flex-row '>
+            <ul className='flex flex-col gap-5'>
+              <li className='text-[20px] font-bold'>Account</li>
+              <li>My Account</li>
+              <li>Cart</li>
+              <li>Wishlist</li>
+              <li>Shop</li>
+            </ul>
+            <ul className='flex flex-col gap-5'>
+              <li className='text-[20px] font-bold'>Quick Link</li>
+              <li>Privacy Policy</li>
+              <li>Terms Of Use</li>
+              <li>FAQ</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          <div className='flex flex-col gap-5 md:mt-0 mt-10'>
+            <h1 className='text-[20px]'>Social </h1>
+            <img className='md:w-auto w-50' src={img4} alt="" />
+          </div>
+        </section>
+
       </footer>
     </>
   )
