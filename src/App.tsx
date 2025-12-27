35db387b6980898696d7686fe2c23ba195903ac4
Abubakr <abubakr9423@gmail.com>
2025-12-27 18:45:53 +0500

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import Checkout from './pages/Checkout'
+import Products from './pages/Products'
+import Productsdetail from './pages/Productsdetail'
+import Wishlist from './pages/Wishlist'
+import Accaount from './pages/Accaount'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Login /> },
+        { path: "/home", element: <Home /> },
+        { path: "/about", element: <About /> },
+        { path: "/cart", element: <Cart /> },
+        { path: "/contact", element: <Contact /> },
+        { path: "/checkout", element: <Checkout /> },
+        { path: "/products", element: <Products /> },
+        { path: "/productsdetail", element: <Productsdetail /> },
+        { path: "/wishlist", element: <Wishlist /> },
+        { path: "/account", element: <Accaount /> }
+      ]
+    }
+  ])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/pages/Productsdetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productsdetail = () => {
+  return (
+    <div>Productsdetail</div>
+  )
+}
+
+export default Productsdetail
